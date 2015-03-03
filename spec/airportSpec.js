d4baddae5b130058def2a5da8a88de79948b0412
wardymate <chriswarddev@gmail.com>
2015-03-03 16:52:57 +0000

    plane status does change on landing fails

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,11 +1,20 @@
-describe(" ", function(){
+describe("Airport ", function(){
   var airport;
+  var plane;
 
-});
+  beforeEach(function() {
+    airport = new Airport();
+    plane = jasmine.createSpyObj('plane', ['land','takeOff']);
+  });
 
-it("can dock a plane", function(){
-  airport = new Airport();
-  airport.dock("plane");
-  expect(airport.planes.count).toEqual(1);
-  
+  it("can dock a plane", function(){
+    airport.dock(plane);
+    expect(airport.planes).toEqual([plane]);
+  });
+
+  it("should change the plane to not flying when it lands", function() {
+    airport.dock(plane);
+    expect(plane.land).toHaveBeenCalled();
+  });
+
 });
---
+++ src/airport.js
@@ -0,0 +1,7 @@
+var Airport = function() {
+  this.planes = [];
+};
+
+Airport.prototype.dock = function(plane) {
+  this.planes.push(plane);
+};
